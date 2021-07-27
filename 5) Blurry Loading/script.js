4f2d73fa63772fda0f7f1c82f046c76a8665ad71
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-07-27 15:27:25 +0300

    added blurry loading

--- 5) Blurry Loading/script.js
+++ 5) Blurry Loading/script.js
@@ -27,3 +27,12 @@ function page() {
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
+
+ /* WHAT WE LEARN IN THIS PROJECT ARE LISTED BELOW...
+  1) filter and blur arrange the blur of an element
+  2) setinterval-clearinterval must used together
+  3) `` using is important while we want to effect innertext
+  4) counter using
+  5) clearinterval must depend on a condition
+  6) setinterval should assign to a variable
+  */ 
